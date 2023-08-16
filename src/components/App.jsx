f8fa63db73df872c322bbc9b9bb9059cdcd1887c
DeveloperStevenMarin <developerstevenmarin@gmail.com>
2023-08-16 00:32:14 -0500

    project finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+import React from 'react';
+import Contacts from './Contacts/Contacts';
+import { useSelector } from 'react-redux';
+import Navigation from './Navigation/Navigation';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 export const App = () => {
+  const user = useSelector(state => state.store.token);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    
+    <Router>
+      <Switch>
+        <Route path="/register">
+          {!user ? <Navigation /> : <Redirect to="/contacts" />}
+        </Route>
+        <Route path="/login">
+          {!user ? <Navigation /> : <Redirect to="/contacts" />}
+        </Route>
+        <Route path="/contacts">
+          {user ? <Contacts /> : <Redirect to="/register" />}
+        </Route>
+        <Redirect from="/" to="/contacts" />
+      </Switch>
+    </Router>
   );
 };
